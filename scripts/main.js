6b6c5f234f56ec5ace2404c664eca44ca662d812
BaptisteVillain <Baptiste@macbook-pro-de-baptiste-3.home>
2016-05-22 14:05:59 +0200

    Popup page artiste

--- scripts/main.js
+++ scripts/main.js
@@ -70,9 +70,69 @@ $(document).ready(function () {
     $('.artisteBackground').removeClass('backgroundActive');
     $('.artistesImg').addClass('ImgActive');
   });
-  
+
   $('#artisteButton').on('mouseleave', function(){
     $('.artisteBackground').addClass('backgroundActive');
     $('.artistesImg').removeClass('ImgActive');
   });
+
+  $('.artisteContainer').on('click', function(){
+    $(this).parent('.col-md-4').addClass('activeArtiste');
+  });
+
+  $('.layerBackground, .closeButton').on('click', function(){
+    $('.layerActiveArtiste').fadeOut();
+    $('.layerBackground').fadeOut();
+    enableScroll();
+  });
+
+  $('.artisteContainer').on('click', function(){
+    $('.layerActiveArtiste').fadeIn();
+    $('.layerBackground').fadeIn();
+    disableScroll();
+  });
+
+  $('.layerActiveArtiste').hide();
+  $('.layerBackground').hide();
+
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+      e.preventDefault();
+    e.returnValue = false;  
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  function disableScroll() {
+    if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove  = preventDefault; // mobile
+    document.onkeydown  = preventDefaultForScrollKeys;
+  }
+
+  function enableScroll() {
+    if (window.removeEventListener)
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+  }
+
+
+
+
+
+
+
 });
