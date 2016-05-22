7d3513a9b1280a50a7abc3c49918d5835286e488
wonooo <toast.nine@gmail.com>
2016-05-23 00:24:42 +0200

    Le php le php

--- contact.php
+++ contact.php
@@ -2,8 +2,8 @@
 
 $emails = ["toast.nine@gmail.com", "elayadinaim@gmail",  "naim.el-ayadi@hetic.net", "gautier.maillard@hetic.net", "baptiste.villain@hetic.net", "kenrig.dalle@hetic.net", "louis.ghodsi@hetic.net"];
 
-$headers = 'FROM: ' . $_POST['mail'];
-mail($emails[$_POST['dest']], 'Formulaire de contact de ', $_POST['fname'] + ' ' + $_POST['lname'], $_POST['comm'], $headers);
+$headers = 'FROM: ' . $_POST['fname'] . $_POST['lname'] . "\r\n" . $_POST['mail'];
+mail($emails[$_POST['dest']], $_POST['subj'], $_POST['comm'], $headers);
 header('Location: contact.html');
 
 ?>
