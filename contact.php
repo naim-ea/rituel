6793d33a183cfa6f46f3dadcfbd7dec1562d7a04
BaptisteVillain <Baptiste@MacBook-Pro-de-Baptiste-3.local>
2016-05-23 17:20:37 +0200

    Je répare mes erreurs

--- contact.php
+++ contact.php
@@ -2,7 +2,6 @@
 
 $errors = array();
 $emails = array('toast.nine@gmail.com', 'elayadinaim@gmail',  'naim.el-ayadi@hetic.net', 'gautier.maillard@hetic.net', 'baptiste.villain@hetic.net', 'kenrig.dalle@hetic.net', 'louis.ghodsi@hetic.net');
-$dest = $_POST['dest'];
 
 
 
@@ -46,7 +45,7 @@ if(!empty($errors)){
 
 
 
-	mail($emails[$dest], $_POST['subj'], $message, $headers);
+	mail($emails[$_POST['dest']], $_POST['subj'], $message, $headers);
 	header('Location: contact.html');
 }
 
