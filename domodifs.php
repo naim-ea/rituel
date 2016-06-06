83f1f510aadf551da65e79348a4bd263300f0a2c
wonooo <toast.nine@gmail.com>
2016-06-06 13:00:04 +0200

    lol ça marche pas

--- domodifs.php
+++ domodifs.php
@@ -1,4 +1,4 @@
-<?php $data = $_POST['jsonString'];
+<?php $data = $_POST['new_data'];
 //set mode of file to writable.
 chmod("data/data.json", 0777);
 $f = fopen("data/data.json", "w+") or die("fopen failed");
